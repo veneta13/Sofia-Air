b93c16ce6bda259d5cd399a8f8456f22493e468b
Veneta <venkireva80@gmail.com>
2022-10-26 22:29:50 +0300

    Add form and format code

--- app/main.py
+++ app/main.py
@@ -1,31 +1,59 @@
 # imports
 import streamlit as st
-import pandas as pd
 import util_funcs
 from content import content
 
-# consts
-lang = 'en'
+con = None
 
-# page info
-st.set_page_config(
-    layout="centered",
-    page_title=content['page_title'][lang],
-    page_icon=":cloud:"
-)
 
-# read data
-df = pd.read_csv('data/sample.csv')
+def form_callback():
+    print(st.session_state)
+    st.session_state['df'] = util_funcs.show_by_location(
+        st.session_state['df'],
+        st.session_state['station_selector']
+    )
 
-# get selector data
-stations = df['station_name'].unique()
 
-# add multiselect
-st.multiselect(
-    content['ams_selector'][lang],
-    options=stations,
-    default=stations[0]
-)
+def main():
+    # consts
+    lang = 'en'
 
-# create map and map stations
-util_funcs.map(df, stations, None)
+    # page info
+    st.set_page_config(
+        layout="centered",
+        page_title=content['page_title'][lang],
+        page_icon=":cloud:"
+    )
+
+    # load initial data
+    st.session_state['df'] = util_funcs.read_data()
+
+    # get selector data
+    stations = st.session_state['df']['station_name'].unique()
+
+    if 'FormSubmitter:map_properties-Get information' not in st.session_state:
+        st.session_state.click = False
+        util_funcs.map(st.session_state['df'], stations, None)
+    else:
+        if st.session_state['FormSubmitter:map_properties-Get information'] == False:
+            util_funcs.map(st.session_state['df'], stations, None)
+        else:
+            util_funcs.map(st.session_state['df'], stations, None)
+
+    with st.form(key='map_properties'):
+
+        st.multiselect(
+            content['ams_selector'][lang],
+            options=stations,
+            default=stations[0],
+            key='station_selector'
+        )
+
+        submit_button = st.form_submit_button(
+            label='Get information',
+            on_click=form_callback()
+        )
+
+
+if __name__ == "__main__":
+    main()
--- app/util_funcs.py
+++ app/util_funcs.py
@@ -1,13 +1,27 @@
+import pandas as pd
 import streamlit as st
-from datetime import datetime
+
+
+@st.experimental_singleton
+def load_data():
+    return pd.read_csv(
+        'data/Archive_Sofia_stations_processed.csv',
+        nrows=10000
+    )
+
+
+def read_data():
+    return load_data()
+
 
 def show_by_location(df, locations):
-    return df[df['station_name'].apply(lambda x : x in locations)]
+    return df[df['station_name'].apply(lambda x: x in locations)]
+
 
 def show_by_time(df, time):
     # print(datetime.fromisoformat(df.iloc[0]['timest']))
     pass
 
+
 def map(df, stations, time):
     return st.map(show_by_location(df, stations))
-
